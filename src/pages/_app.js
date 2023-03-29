a8f05c8fe8beba2c266d9af2e62c1e958118d2af
nad279444 <nardfrimpong@gmail.com>
2023-03-29 16:15:34 +0000

    added a next-auth provider to the auth directory

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import { Provider } from 'next-auth/client'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
+
+
+
